0787ebab31086000c5ab7ae1a0228b63e4940de7
udhayavirat18 <udhayavirat12@kakao.com>
2024-03-30 18:12:04 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+
+// routes
+
+app.get('/', (req,res)=> {
+    res.send('<h1>Hello World</h1>');
+})  
+app.listen(3000,() => {
+    console.log('Server is running on port 3000');
+});
